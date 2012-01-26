858af12c82ba4a0d42fc4fb742660baa594fb930
Jacob Williams <cheapdevotion@gmail.com>
2012-01-26 15:51:58 -0600

    Added support for IE 5, 5.5, and 6

--- Javascript/devpipe.js
+++ Javascript/devpipe.js
@@ -19,7 +19,22 @@ var DevPipe = function(module_options) {
     var port = options.port;
     var project = options.project;
 
-    // Private method
+    //Support for IE 5, 5.5 and 6
+	if (typeof XMLHttpRequest == "undefined")
+	  XMLHttpRequest = function () {
+	    try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+	      catch (e) {}
+	    try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+	      catch (e) {}
+	    try { return new ActiveXObject("Microsoft.XMLHTTP"); }
+	      catch (e) {}
+	    //Microsoft.XMLHTTP points to Msxml2.XMLHTTP and is redundant
+	    throw new Error("This browser does not support XMLHttpRequest.");
+	  };
+
+
+
+	// Private method
     var sendMessage = function(message) {
         var url = "http://" + host + ":" + port + "/project/" + project;
         var json = JSON.stringify(message);
